bd4b769ceee6d3d8ceb26e998994d89d047c98f9
Alexander D Agosta <alexanderagostapa@gmail.com>
2017-07-30 18:33:57 -0700

    write a smoothed version of a slice that can be FFTed in Audacity

--- fft.R
+++ fft.R
@@ -5,7 +5,7 @@ library(signal)
 t <- 1:1024
 
 # Sample with 3 sin components
-x <- 10*sin(2*pi*t*2.3) + 0.14* sin(2*pi*t*8.1+1) + 0.01*sin(2*pi*t/14.1+1) + 0.1*rnorm(length(t))
+x <- 10*sin(2*pi*t*1.3) + 0.64* sin(2*pi*t*8.1+1) + 0.1*sin(2*pi*t/14.1+1) # + 0.1*rnorm(length(t))
 
 # Filter of length 20 points
 g <- gausswin(20)
--- smooth.R
+++ smooth.R
@@ -1,34 +1,46 @@
 
 library(signal)
-library("tuneR")
+library(tuneR)
+source("init.R")
 
-# Number of samples
-t <- 1:1024
-
-# Sample with 3 sin components
-#x <- 10*sin(2*pi*t*2.3) + 0.14* sin(2*pi*t*8.1+1) + 0.01*sin(2*pi*t/14.1+1) + 0.1*rnorm(length(t))
-wave_file <- "slices/slice_01.wav"
+# Bring in a stereo sample
+slice_no <- "01"
+wave_file <- paste0(AUDIO_PATH, "\\slices\\slice_", slice_no, ".wav")
 x <- readWave(wave_file)
-x <- x@left
+xl <- x@left #[300:1600]
 
-plot(x[1:100], type='l')
+plot(xl, type='b', col="navy")
 
 ### SMOOTHER
 # Filter of length 20 points
 g <- gausswin(50)
 # Filtered signal
-z <- x[300:1600]
-f <- stats::filter(z, g)
-plot(z, type='l')
+f <- stats::filter(xl, g, circular=T)            #  circular to avoid NAs at the ends. 
 points(f*0.05, type='l', col='pink', lwd=3)
 
 # Filtered signal
-# Remove endpoints
-f <- f[!is.na(f)]
-write.csv(f, "Sm_slice1.csv" )
+write.table(f, paste0(AUDIO_PATH, "\\slices\\smooth_slice_", slice_no, ".csv"), row.names = F, col.names = F )
 
-# Fast Fourier transform
+# Fast Fourier transform  -- not very effective, 
 t0<- floor(length(f)/2):length(f)
 plot(t0, Mod(fft(f))[t0], type='l', col='darkred')
 
+# Create an object to export for FFT
+w <- normalize(Wave(left = as.numeric(round(f)),samp.rate=8000, bit =16), unit="16")
+writeWave(w, paste0(AUDIO_PATH, "\\slices\\wsmooth_", slice_no, ".wav"))
+#When loaded into Audacity, the peaks for slice 01 are
+# Frequency (Hz)	Level (dB)
+# 7.812500	-44.705215
+# 15.625000	-40.179810
+# 23.437500	-28.621590
+# 31.250000	-19.819921
+# -> 39.062500	-17.139282
+# 46.875000	-21.965178
+# 54.687500	-33.083889
+# 62.500000	-26.046791
+# 70.312500	-23.299898
+# 78.125000	-20.104364
+# -> 85.937500	-19.944099
+# 93.750000	-20.344444
+# 101.562500	-21.880117
 
